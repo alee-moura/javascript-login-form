ab2b771246f8ce5e420f1cd288080d0171b910de
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-04-03 22:39:56 -0300

    remove bugs

--- js/index.js
+++ js/index.js
@@ -1,56 +1,58 @@
 const init = () => {
     const validateEmail = (event) => {
-        const input = event.currentTarget
+        const input = event.currentTarget;
         const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         const emailTest = regex.test(input.value);
 
         if(!emailTest) {
-            submitButton.setAttribute('disable', 'disabled');
+            submitButton.setAttribute("disabled", "disabled");
             input.nextElementSibling.classList.add('error');
         } else {
-            submitButton.removeAttribute('disabled');
+            submitButton.removeAttribute("disabled");
             input.nextElementSibling.classList.remove('error');
         }
     }
 
-    const validadePassword = (event) => {
+    const validatePassowrd = (event) => {
         const input = event.currentTarget;
 
         if(input.value.length < 8) {
-            submitButton.setAttribute('disabled', 'disabled');
+            submitButton.setAttribute("disabled", "disabled");
             input.nextElementSibling.classList.add('error');
         } else {
-            submitButton.removeAttribute('disabled');
+            submitButton.removeAttribute("disabled");
             input.nextElementSibling.classList.remove('error');
         }
     }
-
-    const inputEmail = document.querySelector('input[type="email"]')
-    const inputPassword = document.querySelector('input[type="password"]')
-    const submitButton = document.querySelector('.login__submit')
+    
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('.login__submit');
 
     inputEmail.addEventListener('input', validateEmail);
-    inputPassword.addEventListener('input', validadePassword);
+    inputPassword.addEventListener('input', validatePassowrd);
 
     const errorHandler = () => {
+        submitButton.classList.remove('loading');
         submitButton.classList.remove('success');
         submitButton.classList.add('error');
-        submitButton.textContent = 'Error :('
+        submitButton.textContent = "Error :(";
     }
 
     const successHandler = () => {
+        submitButton.classList.remove('loading');
         submitButton.classList.remove('error');
         submitButton.classList.add('success');
-        submitButton.textContent = 'Sent! :)'
+        submitButton.textContent = "Sent! :)";
     }
 
     if(submitButton) {
         submitButton.addEventListener('click', (event) => {
             event.preventDefault();
 
-            submitButton.textContent = '...Loading';
-        
-            fetch('https://regres.in/api/login', {
+            submitButton.textContent = "Loading...";
+
+            fetch('https://reqres.in/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -63,7 +65,9 @@ const init = () => {
                 if (response.status !== 200) {
                     return errorHandler();
                 }
+                
                 successHandler();
+                
             }).catch(() => {
                 errorHandler();
             })
