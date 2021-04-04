2a23deaaa4d5f10a27b8810c3e00be9deebe0b23
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-04-03 22:03:46 -0300

    requisition api

--- js/index.js
+++ js/index.js
@@ -2,6 +2,27 @@ const init = () => {
     const inputEmail = document.querySelector('input[type="email"]')
     const inputPassword = document.querySelector('input[type="password"]')
     const submitButton = document.querySelector('.login__submit')
+
+    if(submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+        
+            fetch('https://regres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) => {
+                return response.json();           
+            }).then((data) => {
+
+            })
+        })
+    }
 }
 
 window.onload = init;
