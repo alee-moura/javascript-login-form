6a0f9a50ce688a184d605c322f2bdad5c21b6af6
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-04-03 21:53:49 -0300

    starting javascript

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]')
+    const inputPassword = document.querySelector('input[type="password"]')
+    const submitButton = document.querySelector('.login__submit')
+}
+
+window.onload = init;
